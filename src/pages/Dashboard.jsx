5be61a95defe7d1e8ebb913edea2a7fd1c383c4e
CodeWithJitender <jitender.developer786@gmail.com>
2025-07-09 17:52:39 +0530

    backend merged into jitender

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -25,7 +25,7 @@ function Dashboard() {
   const perPage = 25;
   const bidsSectionRef = useRef(null);
   const [count, setCount] = useState(0);
-  const [sidebarToggle, setSidebarToggle] = useState(true);
+  const [sidebarToggle, setSidebarToggle] = useState(false);
   const navigate = useNavigate();
   const middle = [
     { id: 1, title: "Total Bids", num: totalResults },
